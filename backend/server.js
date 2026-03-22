701941e5294a95044281333b6f8b8450a8ef0ef1
dkav0206 <dkhoa2019vn@gmail.com>
2026-03-22 16:39:30 +1000

    github actions

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ mongoose.connect(process.env.MONGO_URI, {
 app.get("/api/get-product", (req, res) => {
     let data = [{
         id: 1,
-        name: "product 1"
+        name: "product 12321312321123"
     },
     {
         id: 2,
